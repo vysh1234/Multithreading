dc01b7bf1ecda7b7f900ac62d09c3b969c4a804e
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 13:20:04 +0530

    multithreading

---
+++ Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Multithreading
+{
+    class Class
+    {
+        public void Display()
+        {
+            Console.WriteLine("implementing thread 1");
+
+            for(int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("\n {0}", i);
+            }
+            Console.WriteLine("Thread 1 closes");
+        }
+        public void Read()
+        {
+            Console.WriteLine("implementing thread 2");
+            for(char j = 'A'; j <= 'Z'; j++)
+            {
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("thread 2 ends");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementing multithreading!");
+
+            Class History = new Class();
+            Console.WriteLine("Main Thread started..");
+            History.Display();
+
+            ThreadStart th1 = History.Display;
+            ThreadStart th2 = History.Read;
+
+            Thread thread1 = new Thread(th1);
+            Thread thread2 = new Thread(th2);
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.WriteLine("Main thread ends here");
+            Console.ReadKey();
+
+        }
+    }
+}
